94b3b1ac1c0bdfeb717b6a890022ca666c742833
Mohit K. Yadav <mohitkyadav@outlook.com>
2020-11-13 16:19:43 +0530

    remove layout

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,18 +1,15 @@
 import React, {FC} from 'react';
 import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
 
-import {Layout} from 'components';
 import Home from './Home';
 
 const App: FC = () => {
   return (
     <Router>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Redirect to="/" />
-        </Switch>
-      </Layout>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Redirect to="/" />
+      </Switch>
     </Router>
   );
 };
