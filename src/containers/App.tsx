877e5db646abb4b26ceb258f762cc7ead13e2f30
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2023-01-22 20:18:22 +0100

    Upgrade react-router-dom to v6

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,15 +1,14 @@
 import React, {FC} from 'react';
-import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import Home from './Home';
 
 const App: FC = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Redirect to="/" />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
 };
